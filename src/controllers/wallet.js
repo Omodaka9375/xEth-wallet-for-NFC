6c4671c60d75c4d4c18d83040aa52657f71cd3f7
Omodaka9375 <branislav.djalic@gmail.com>
2020-05-23 01:30:47 +0200

    .

--- src/controllers/wallet.js
+++ src/controllers/wallet.js
@@ -1,6 +1,4 @@
 import * as WalletService from '../services/wallet';
-import logger from '../utils/logger';
-
 
 const create = (req, res) => res.send(WalletService.create());
 
@@ -12,13 +10,10 @@ const getBalance = ({ params: { ethAddress = null } }, res) => {
 };
 
 const transaction = ({ req }, res) => {
-  logger.info(req.amount);
   WalletService.transaction(req).then(
-
     transaction => res.send(transaction),
     error => res.status(error.errorCode || 200).send(error)
   );
 };
-
 
 export { create, getBalance, transaction };
